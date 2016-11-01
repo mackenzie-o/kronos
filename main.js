d41ec384ae7b6b7674a9dc05c082fabbf645ad21
MacKenzie O'Bleness <mackenzie.obleness@gmail.com>
2016-11-01 17:03:12 -0600

    added nifty slider

--- main.js
+++ main.js
@@ -48,4 +48,32 @@ function updateAudio(playback) {
 	audio.play();
 }
 
-window.onload = setPlayback(100);
+function init() {
+	console.log("eyyy");
+	$('input[type="range"]').rangeslider({
+		polyfill: false,
+
+	    // Callback function
+	    onInit: function() {
+	    	console.log("I did it!")
+	    },
+
+	    // Callback function
+	    onSlide: function(position, value) {
+	    	console.log("slide called: " + position + ", " + value);
+	    },
+
+	    // Callback function
+	    onSlideEnd: function(position, value) {
+	    	console.log("slideEnd called: " + position + ", " + value);
+	    	updateAudio(value);
+	    }
+	});
+}
+
+function slideEnd(position, value) {
+	updateAudio(value);
+}
+
+$(document).ready(init);
+
